9bbe98f10da6fc28f9bd934486cb73cf2b192e8a
Janchorizo <jancho@usal.es>
2021-02-14 12:02:46 +0100

    Handle suscription commands.

--- src/handlers.py
+++ src/handlers.py
@@ -125,19 +125,31 @@ class SearchHandler:
 
 class SuscriptionHandler:
     def handles(self, update):
-        if update.type != types.CallbackQuery:
-            return False
-        if update.content['data'].startswith(self.__class__.__name__):
+        if (update.type == types.CallbackQuery and
+                update.content['data'].startswith(self.__class__.__name__)):
+            return True
+        if (update.type == types.Message and 
+                update.content.is_command('/suscribirse')):
+            return True
+        if (update.type == types.Message and 
+                update.content.is_command('/desuscribirse')):
             return True
         return False
     
     def __call__(self, update, token):
-        msg = messages.CaptionReplacementContent(
-            message_id=update.content['message']['message_id'],
-            caption='[TODO] ' + self.__class__.__name__,
-            parse_mode='MarkdownV2',
-            reply_markup='')
-        msg.apply(token, update.content.cid, verbose=True)
+        if (update.type == types.CallbackQuery):
+            msg = messages.CaptionReplacementContent(
+                message_id=update.content['message']['message_id'],
+                caption='A partir de ahora *sí* recibirás el BOE automáticamente\\.',
+                parse_mode='MarkdownV2',
+                reply_markup='')
+            msg.apply(token, update.content.cid, verbose=True)
+        else:
+            option = 'sí' if update.content.is_command('/suscribirse') else 'no'
+            msg = messages.MessageContent(
+                text=f'A partir de ahora *{option}* recibirás el BOE automáticamente\\.',
+                parse_mode='MarkdownV2')
+            msg.send(token, update.content.cid, verbose=True)
 
 
 help_text = (
