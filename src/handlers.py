e4529e55ba376ea3d179d412058f3333834966ad
Janchorizo <jancho@usal.es>
2021-02-22 12:10:36 +0100

    Add summary to day handler and set last available day.

--- src/handlers.py
+++ src/handlers.py
@@ -4,6 +4,9 @@ import datetime, calendar
 import locale
 import json
 
+import pymongo
+from pymongo import MongoClient
+
 from telegram import messages
 from telegram import types
 from telegram import methods
@@ -16,6 +19,8 @@ basedir = os.path.dirname(os.path.abspath(__file__))
 
 
 class DayHandler:
+    collection = 'diary_summary'
+
     def handles(self, update):
         if update.type != types.CallbackQuery:
             return False
@@ -23,10 +28,42 @@ class DayHandler:
             return True
         return False
 
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
+        year, month, day = update.content['data'].split(':')[1:]
+        year, month, day = int(year), int(month), int(day)
+        date = datetime.datetime(year, month, day)
+        formatted_date = '{:%Y-%m-%d}'.format(date)
+
+        client = MongoClient(dburi)
+        db = client[dbname]
+        summary = db[self.collection].find_one({'date': formatted_date})
+        client.close()
+        if summary == None:
+            return
+        
+        formatted_link = summary["link"]\
+            .replace(".", "\\.")\
+            .replace('=', '\\=')\
+            .replace('-', '\\-')\
+            .replace('_', '\\_')
+        formatted_entry_types = '\n'.join(
+            f'◇ {c} entradas son {t}'
+            for t, c
+            in summary["per_type_def_count"].items()
+            if t != ''
+        )
+
+        caption = (
+            f'Boletín del día *{day} de {calendar.month_name[month].capitalize()}, {year}*\\.'
+            f'Accesible en {formatted_link}\\.\n\n'
+            f'Se registraron un total de {summary["entry_count"]} entradas, de las cuales:\n'
+            f'{formatted_entry_types}'
+        )
+
+        print(caption)
         msg = messages.CaptionReplacementContent(
             message_id=update.content['message']['message_id'],
-            caption='[TODO] ' + update.content['data'],
+            caption=caption,
             parse_mode='MarkdownV2',
             reply_markup='')
         msg.apply(token, update.content.cid, verbose=True)
@@ -61,7 +98,7 @@ class DayInputHandler:
             return True
         return False
 
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
         fields = update.content['data'].split(':')
         field_count = len(fields) - 1
         year = None if field_count == 0 else int(fields[1])
@@ -119,7 +156,7 @@ class SearchHandler:
             return True
         return False
 
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
         search_string = update.content["text"].split('/buscar')[1].strip()
         msg = messages.MessageContent(
             text=f'_Buscando_ {search_string} \\. \\. \\.',
@@ -140,7 +177,7 @@ class SuscriptionHandler:
             return True
         return False
     
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
         if (update.type == types.CallbackQuery):
             msg = messages.CaptionReplacementContent(
                 message_id=update.content['message']['message_id'],
@@ -182,7 +219,7 @@ class HelpHandler:
                 return True
         return False
     
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
         with open(os.path.join(basedir, 'static/header.png'), 'rb') as p:
             start_msg = messages.PhotoContent(
                 photo=p,
@@ -200,8 +237,8 @@ menu_text = (
 )
 
 
-menu_options = json.dumps({'inline_keyboard': [
-    [{'text': 'Ver el último BOE', 'callback_data': DayHandler.__name__}],
+menu_options = lambda last_day: json.dumps({'inline_keyboard': [
+    [{'text': f'Ver el último BOE ({last_day:%d/%m/%Y})', 'callback_data': f'{DayHandler.__name__}:{last_day:%Y:%m:%d}'}],
     [{'text': 'Ver otro día', 'callback_data': DayInputHandler.__name__}],
     [{'text': 'Buscar en el BOE', 'switch_inline_query_current_chat': '/buscar '}],
     [{'text': 'Suscribirse al BOE', 'callback_data': SuscriptionHandler.__name__}],
@@ -209,19 +246,32 @@ menu_options = json.dumps({'inline_keyboard': [
 
 
 class MenuHandler:
+    collection = 'diary_summary'
+
     def handles(self, update):
         if update.type == types.Message:
             if update.content.is_command('/menu') or update.content.mentions('@boes_bot'):
                 return True
         return False
     
-    def __call__(self, update, token):
+    def __call__(self, update, token, dbname, dburi):
+        client = MongoClient(dburi)
+        db = client[dbname]
+        cursor = db[self.collection].find({}, {'date': 1})\
+            .sort([('date', pymongo.DESCENDING)])\
+            .limit(1)
+        
+        year, month, day = next(cursor)['date'].split('-')
+        year, month, day = int(year), int(month), int(day)
+        last_day = datetime.datetime(year, month, day)
+        client.close()
+
         with open(os.path.join(basedir, 'static/header.png'), 'rb') as p:
             start_msg = messages.PhotoContent(
                 photo=p,
                 parse_mode='MarkdownV2',
                 caption=menu_text,
-                reply_markup=menu_options)
+                reply_markup=menu_options(last_day))
             start_msg.send(token, update.content.cid, verbose=True)
 
 
