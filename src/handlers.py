58ca7a7d0a06afec9887bdf6ff10bbe186a1707e
Janchorizo <jancho@usal.es>
2021-02-25 23:16:51 +0100

    Restrict eligible days.

--- src/handlers.py
+++ src/handlers.py
@@ -108,7 +108,7 @@ class DayHandler:
                     os.remove(local_path)
 
 
-def get_arranged_buttons(year, month, fmt, fallback):
+def get_arranged_buttons(year, month, bounds, fmt, fallback):
     c = calendar.TextCalendar()
     f_width = 3
     weeks = c.formatmonth(year, month, w=f_width).split('\n')[2:-1] # remove headers
@@ -117,6 +117,15 @@ def get_arranged_buttons(year, month, fmt, fallback):
         for week
         in weeks
     ]
+    filtered_days = [
+        [
+            day if int(day) >= bounds[0] and int(day) <= bounds[1] else ''
+            for day
+            in week
+        ]
+        for week
+        in arranged_days
+    ]
     arranged_buttons = [
         [
             fallback if day == '' else {'text': day, 'callback_data': fmt.format(year, month, day)}
@@ -124,12 +133,14 @@ def get_arranged_buttons(year, month, fmt, fallback):
             in week
         ]
         for week
-        in arranged_days
+        in filtered_days
     ]
     return arranged_buttons
 
 
 class DayInputHandler:
+    collection = 'diary_summary'
+
     def handles(self, update):
         if update.type != types.CallbackQuery:
             return False
@@ -143,8 +154,20 @@ class DayInputHandler:
         year = None if field_count == 0 else int(fields[1])
         month = None if field_count < 2 else int(fields[2])
 
+        client = MongoClient(dburi)
+        db = client[dbname]
+        cursor = db[self.collection].aggregate([
+            {'$group': {'_id': '', 'max': {'$max': '$date'}, 'min': {'$min': '$date'}}}
+        ])
+        
+        date_range = next(cursor)
+        client.close()
+
+        first_date = datetime.datetime(*map(int, date_range['min'].split('-')))
+        last_date = datetime.datetime(*map(int, date_range['max'].split('-')))
+
         if field_count == 0: # select a year
-            years = (2021,)
+            years = [*range(first_date.year, last_date.year+1)]
             caption = 'Selecciona el año que quieres consultar\\.'
             options = json.dumps({'inline_keyboard': [
                 [{'text': y, 'callback_data': f'{DayInputHandler.__name__}:{y}'}
@@ -152,8 +175,19 @@ class DayInputHandler:
             ]})
 
         if field_count == 1: # select the month
-            months = tuple(range(1,13))
-            arranged_months = [months[i*3:i*3+3] for i in range(len(months)//3)]
+            bounds = [1, 13]
+            if year == first_date.year:
+                bounds[0] = first_date.month
+            if year == last_date.year:
+                bounds[1] = last_date.month + 1
+            
+            months = tuple(range(*bounds))
+            row_count = max(1, len(months)//3)
+            arranged_months = []
+            for i in range(row_count):
+                left = 3 if i < row_count+1 else row_count%3
+                arranged_months.append(months[i*3:i*3+left])
+
             caption = (f'Selecciona el mes de *{year}* que quieres consultar\\.\n'
                         '_También puedes volver atrás para cambiar de año_\\.')
             options = json.dumps({'inline_keyboard': [
@@ -167,6 +201,12 @@ class DayInputHandler:
             
 
         if field_count == 2: # select the day
+            bounds = [1, calendar.mdays[month]]
+            if year == first_date.year and month == first_date.month:
+                bounds[0] = first_date.day
+            if year == last_date.year and month == last_date.month:
+                bounds[1] = last_date.day
+
             fmt = DayHandler.__name__+':{}:{}:{}'
             fallback = {'text': ' ', 'callback_data': f'{DayInputHandler.__name__}:{year}:{month}'}
             
@@ -174,7 +214,7 @@ class DayInputHandler:
                         ' que quieres consultar\\.\n'
                         '_También puedes volver atrás para cambiar de mes_\\.')
             
-            buttons = get_arranged_buttons(year, month, fmt, fallback)
+            buttons = get_arranged_buttons(year, month, bounds, fmt, fallback)
             buttons.append([{'text': 'Atrás', 'callback_data': f'{DayInputHandler.__name__}:{year}'}])
             options = json.dumps({'inline_keyboard': buttons})
             
